7fbf58635d0dfeba71bf5c64695d1c16e7635b38
Moolmancell <richmondclaudebodot@gmail.com>
2023-08-14 20:08:33 +0800

    Answer 09 Palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,6 @@
-const palindromes = function () {
-
+const palindromes = function(sentence) {
+    const rawWord = sentence.replace(/[^A-Z0-9]+/ig, "").toLowerCase();
+    return rawWord.split("").reduceRight((word, current) => word + current, "") === rawWord
 };
 
 // Do not edit below this line
