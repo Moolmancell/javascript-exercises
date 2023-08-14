6b12087381ca81f1f17baccad7bb9ea41d21389d
Moolmancell <richmondclaudebodot@gmail.com>
2023-08-14 19:09:19 +0800

    Answer room activity 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,29 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a+b
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a-b
 };
 
-const sum = function() {
-	
+const sum = function(set) {
+	return set.reduce((number, item) => number + item, 0)
 };
 
-const multiply = function() {
-
+const multiply = function(...set) {
+  return set.reduce((number, item) => number * item)
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return a**b
 };
 
-const factorial = function() {
-	
+const factorial = function(set) {
+  let total = 1;
+	for (;set != 0;set--){
+    total*=set
+  }
+  return total
 };
 
 // Do not edit below this line
